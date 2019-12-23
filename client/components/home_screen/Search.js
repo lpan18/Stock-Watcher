38de6fb9ba562b3ebefa1234047eef1624d563b3
lpan <sophie.leipan@gmail.com>
2019-12-22 21:39:03 -0800

    save temp

--- client/components/home_screen/Search.js
+++ client/components/home_screen/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { SearchBar } from "react-native-elements"
 import { StyleSheet, View, Text } from "react-native"
 import Touchable from 'react-native-platform-touchable'
@@ -6,87 +6,101 @@ import * as WebBrowser from 'expo-web-browser'
 
 import { useQuery } from "@apollo/react-hooks"
 import { GET_PROFILE } from "../queries"
-// import RNFS from 'react-native-fs'
-var RNFS = require('react-native-fs');
+import * as FileSystem from 'expo-file-system';
+import * as R from 'ramda'
+import SYMBOLS from '../../resources/SYMBOLS'
 
-// function getAllSymbols(symbolsFilePath){
-//   var path = RNFS.DocumentDirectoryPath + symbolsFilePath;
-//   console.log(path)
-//   RNFS.readFile(path, 'utf8')
-//   .then((res)=>{
-//     console.log('read file', res);
-//   })
-//   .catch((err) => {
-//     console.log(err.message, err.code);
-//   });
+// const getAllSymbols = async () => {
+//   const sourceUri = 'https://financialmodelingprep.com/api/v3/company/stock/list';
+//   const destUri = FileSystem.documentDirectory + 'symbol.csv';
+//   return FileSystem.downloadAsync(sourceUri, destUri)
+//     .then(({ uri }) => {
+//       console.log('Finished downloading to ', uri);
+//       return FileSystem.readAsStringAsync(uri).then(res => JSON.parse(res).symbolsList);
+//     })
+//     .catch(error => {
+//       console.error(error);
+//     });
 // }
 
+const getMatchedSymbols = (symbols, searchTxt) => {
+  return R.filter(x => x.startsWith(searchTxt), symbols);//R.map(x => x.symbol, symbolsList));
+};
+
+const getProfiles = async (symbols) => {
+  const requests = symbols.map((symbol) => {
+    const url = `https://financialmodelingprep.com/api/v3/company/profile/${symbol}`
+    return fetch(url)
+      .then(a => {
+        return a.json()
+      })
+  })
+  return Promise.all(requests)
+}
+
 export default function Search(props) {
-  // const { navigate } = props.navigation;
+  const { navigate } = props.navigation;
   const [searchTxt, setSearchTxt] = useState("AAPL");
+  const [matchedSymbols, setMatchedSymbols] = useState(["AAPL"]);
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
   // let shouldSkip = false;
   let maxSymbolLength = 6;
   const updateSearchTxt = searchTxt => {
     if (searchTxt.length <= maxSymbolLength) {
       setSearchTxt(searchTxt);
+      setMatchedSymbols(getMatchedSymbols(SYMBOLS, searchTxt).slice(0, 2)); // slice to 2 to reduce api call, for dev purpose  
+    };
+    const handlePressStock = () => {
+      props.navigation.navigate('StockDetail', {
+        symbol: searchTxt
+      })
     }
-  };
-  const symbolsFilePath = '../../resources/symbol.csv';
-  // getAllSymbols(symbolsFilePath);
 
-  const handlePressStock = () => {
-    props.navigation.navigate('StockDetail',  {
-      symbol: searchTxt
-    })
-  }
-  // if (searchTxt == "AAPL" || searchTxt == "MSFT") {
-  //   shouldSkip = false;
-  // }else{
-  //   shouldSkip = true;
-  // }
-  // const { loading, error, data } = useQuery(GET_PROFILE, {
-  //   // skip: shouldSkip,
-  //   variables: { symbol: searchTxt }
-  // });
+    useEffect(() => {
+      getProfiles(matchedSymbols).then(res => {
+        setData(res);
+        setIsLoading(false);
+      })
+    }, [matchedSymbols]);
 
-  // if (error) {
-  //   return <Text>Error! {error.message}</Text>;
-  // }
-
-  return (
-    <View>
-      <SearchBar
-        lightTheme
-        placeholder="Search Here..."
-        onChangeText={updateSearchTxt}
-        value={searchTxt}
-        showCancel
-      />
-      {/* {loading || searchTxt == "" ? <Text /> : (
-        <View style={styles.stockView}>
-        <Text style={styles.symbolText}>Symbols</Text>
-        <Touchable onPress={handlePressStock}>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <View style={styles.stockView}>
-              <Text style={styles.stockTitleText}>{data.security.symbol}
-              </Text>
-              <Text style={styles.stockBodyText}>{data.security.profile.companyName}
-              </Text>
-            </View>
-            <View style={styles.stockView}>
-              <Text style={styles.stockTitleText}>{data.security.profile.price}
-              </Text>
-              <Text style={styles.stockBodyText}>{data.security.profile.changesPercentage}
-              </Text>
-
-            </View>
+    return (
+      <View>
+        <SearchBar
+          lightTheme
+          placeholder="Search Here..."
+          onChangeText={updateSearchTxt}
+          value={searchTxt}
+          showCancel
+        />
+        {isLoading ? <Text /> : (
+          <View style={styles.stockView}>
+            <Text style={styles.symbolText}>Symbols</Text>
+            {data.map(d => (
+              <Touchable onPress={handlePressStock} key={d.symbol}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                  <View style={styles.stockView}>
+                    <Text style={styles.stockTitleText}>{d.symbol}
+                    </Text>
+                    <Text style={styles.stockBodyText}>{d.profile.companyName}
+                    </Text>
+                  </View>
+                  <View style={styles.stockView}>
+                    <Text style={styles.stockTitleText}>{d.profile.price}
+                    </Text>
+                    <Text style={styles.stockBodyText}>{d.profile.changesPercentage}
+                    </Text>
+                  </View>
+                </View>
+              </Touchable>
+            ))}
           </View>
-        </Touchable>
-        </View>
-      )
-      } */}
-    </View>
-  );
+        )
+        }
+      </View>
+    );
+  }
 }
 
 Search.navigationOptions = {
@@ -97,7 +111,7 @@ const styles = StyleSheet.create({
   stockView: {
     margin: 10
   },
-  symbolText:{
+  symbolText: {
     fontSize: 30,
     fontWeight: 'bold'
   },
