a48883941f7a2c22c250d9dd5e4b082f3b3c1ce9
lpan <sophie.leipan@gmail.com>
2019-12-22 21:42:58 -0800

    finish search match

--- client/components/home_screen/Search.js
+++ client/components/home_screen/Search.js
@@ -2,26 +2,8 @@ import React, { useState, useEffect } from "react"
 import { SearchBar } from "react-native-elements"
 import { StyleSheet, View, Text } from "react-native"
 import Touchable from 'react-native-platform-touchable'
-import * as WebBrowser from 'expo-web-browser'
-
-import { useQuery } from "@apollo/react-hooks"
-import { GET_PROFILE } from "../queries"
-import * as FileSystem from 'expo-file-system';
 import * as R from 'ramda'
 import SYMBOLS from '../../resources/SYMBOLS'
-
-// const getAllSymbols = async () => {
-//   const sourceUri = 'https://financialmodelingprep.com/api/v3/company/stock/list';
-//   const destUri = FileSystem.documentDirectory + 'symbol.csv';
-//   return FileSystem.downloadAsync(sourceUri, destUri)
-//     .then(({ uri }) => {
-//       console.log('Finished downloading to ', uri);
-//       return FileSystem.readAsStringAsync(uri).then(res => JSON.parse(res).symbolsList);
-//     })
-//     .catch(error => {
-//       console.error(error);
-//     });
-// }
 
 const getMatchedSymbols = (symbols, searchTxt) => {
   return R.filter(x => x.startsWith(searchTxt), symbols);//R.map(x => x.symbol, symbolsList));
@@ -39,68 +21,66 @@ const getProfiles = async (symbols) => {
 }
 
 export default function Search(props) {
-  const { navigate } = props.navigation;
   const [searchTxt, setSearchTxt] = useState("AAPL");
   const [matchedSymbols, setMatchedSymbols] = useState(["AAPL"]);
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
-  // let shouldSkip = false;
   let maxSymbolLength = 6;
   const updateSearchTxt = searchTxt => {
     if (searchTxt.length <= maxSymbolLength) {
       setSearchTxt(searchTxt);
       setMatchedSymbols(getMatchedSymbols(SYMBOLS, searchTxt).slice(0, 2)); // slice to 2 to reduce api call, for dev purpose  
     };
-    const handlePressStock = () => {
-      props.navigation.navigate('StockDetail', {
-        symbol: searchTxt
-      })
-    }
-
-    useEffect(() => {
-      getProfiles(matchedSymbols).then(res => {
-        setData(res);
-        setIsLoading(false);
-      })
-    }, [matchedSymbols]);
-
-    return (
-      <View>
-        <SearchBar
-          lightTheme
-          placeholder="Search Here..."
-          onChangeText={updateSearchTxt}
-          value={searchTxt}
-          showCancel
-        />
-        {isLoading ? <Text /> : (
-          <View style={styles.stockView}>
-            <Text style={styles.symbolText}>Symbols</Text>
-            {data.map(d => (
-              <Touchable onPress={handlePressStock} key={d.symbol}>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                  <View style={styles.stockView}>
-                    <Text style={styles.stockTitleText}>{d.symbol}
-                    </Text>
-                    <Text style={styles.stockBodyText}>{d.profile.companyName}
-                    </Text>
-                  </View>
-                  <View style={styles.stockView}>
-                    <Text style={styles.stockTitleText}>{d.profile.price}
-                    </Text>
-                    <Text style={styles.stockBodyText}>{d.profile.changesPercentage}
-                    </Text>
-                  </View>
-                </View>
-              </Touchable>
-            ))}
-          </View>
-        )
-        }
-      </View>
-    );
   }
+  const handlePressStock = () => {
+    props.navigation.navigate('StockDetail', {
+      symbol: searchTxt
+    })
+  }
+
+  useEffect(() => {
+    getProfiles(matchedSymbols).then(res => {
+      setData(res);
+      setIsLoading(false);
+    })
+  }, [matchedSymbols]);
+
+  return (
+    <View>
+      <SearchBar
+        lightTheme
+        placeholder="Search Here..."
+        onChangeText={updateSearchTxt}
+        value={searchTxt}
+        showCancel
+      />
+      {isLoading ? <Text /> : (
+        <View style={styles.stockView}>
+          <Text style={styles.symbolText}>Symbols</Text>
+          {data.map(d => (
+            <Touchable onPress={handlePressStock} key={d.symbol}>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <View style={styles.stockView}>
+                  <Text style={styles.stockTitleText}>{d.symbol}
+                  </Text>
+                  <Text style={styles.stockBodyText}>{d.profile.companyName}
+                  </Text>
+                </View>
+                <View style={styles.stockView}>
+                  <Text style={styles.stockTitleText}>{d.profile.price}
+                  </Text>
+                  <Text style={styles.stockBodyText}>{d.profile.changesPercentage}
+                  </Text>
+                </View>
+              </View>
+            </Touchable>
+          ))}
+        </View>
+      )
+      }
+    </View>
+  );
 }
 
 Search.navigationOptions = {
